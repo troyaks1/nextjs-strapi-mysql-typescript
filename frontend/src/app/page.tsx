11d5cae9befa2a4f08eeaa782f36a4ec918ecc1c
troyaks1 <lgotcfg@gmail.com>
2024-08-22 17:16:28 -0300

    First commit, setting up frontend with next.js, backend with strapi and mysql.

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-6">
+      <Button>
+        Click me, I don't do anything
+      </Button>
+    </main>
+  )
+}
