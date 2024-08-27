d91d3d1aa22fabbb3d82d4cc21fe66a003922405
troyaks <lgotcfg@gmail.com>
2024-08-27 00:06:36 -0300

    Starting template home-page creation.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,28 @@
 import { Button } from "@/components/ui/button"
 
-export default function Home() {
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+  try {
+    const res = await fetch(baseUrl + path);
+    const data = await res.json();
+    if (data.error) {
+      throw new Error(data.error.message);
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+  console.log(strapiData)
+  const { title, description } = strapiData.data.attributes;
+
   return (
     <main className="container mx-auto py-6">
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
       <Button>
         Click me, I don't do anything
       </Button>
