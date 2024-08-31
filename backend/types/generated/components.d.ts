d75971192d2561c1f1555b0ad9c1ec4aa7df7bb4
troyaks <lgotcfg@gmail.com>
2024-08-30 21:47:42 -0300

    Creating features section and hero section.

---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,63 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    image: Attribute.Media<'images'>;
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutFeaturesSection extends Schema.Component {
+  collectionName: 'components_layout_features_sections';
+  info: {
+    displayName: 'Features Section';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    feature: Attribute.Component<'components.feature', true>;
+  };
+}
+
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Attribute.String;
+    text: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ComponentsFeature extends Schema.Component {
+  collectionName: 'components_components_features';
+  info: {
+    displayName: 'Feature';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    icon: Attribute.Enumeration<['CLOCK_ICON', 'CHECK_ICON', 'CLOUD_ICON']>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'layout.hero-section': LayoutHeroSection;
+      'layout.features-section': LayoutFeaturesSection;
+      'components.link': ComponentsLink;
+      'components.feature': ComponentsFeature;
+    }
+  }
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,31 +1,29 @@
-import { Button } from "@/components/ui/button"
+import { getHomePageData } from "@/data/loaders";
 
-async function getStrapiData(path: string) {
-  const baseUrl = "http://localhost:1337";
-  try {
-    const res = await fetch(baseUrl + path);
-    const data = await res.json();
-    if (data.error) {
-      throw new Error(data.error.message);
-    }
-    return data;
-  } catch (error) {
-    console.error(error);
+import { HeroSection } from "@/components/custom/HeroSection";
+import { FeatureSection } from "@/components/custom/FeaturesSection";
+
+function blockRenderer(block: any) {
+  switch (block.__component) {
+    case "layout.hero-section":
+      return <HeroSection key={block.id} data={block} />;
+    case "layout.features-section":
+      return <FeatureSection key={block.id} data={block} />;
+    default:
+      return null;
   }
 }
 
 export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page");
-  console.log(strapiData)
-  const { title, description } = strapiData.data.attributes;
+  const strapiData = await getHomePageData();
+
+  const { blocks } = strapiData;
+
+  if (!blocks) return <div>No data found from Strapi.</div>
 
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-5xl font-bold">{title}</h1>
-      <p className="text-xl mt-4">{description}</p>
-      <Button>
-        Click me, I don't do anything
-      </Button>
+    <main>
+      {blocks.map((block: any) => blockRenderer(block))}
     </main>
-  )
+  );
 }
